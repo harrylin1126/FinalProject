540a9a0e738719f4e73ba550bfd2501affcf31bc
yourfurmushroom <Zihui_0411@hotmail.com>
2024-11-13 23:38:42 +0800

    change dynamic link video

--- baseball/src/Component/ResultArea.js
+++ baseball/src/Component/ResultArea.js
@@ -1,38 +1,76 @@
+// import { useEffect, useState } from "react";
+// import {ws} from "./WebServer"
+// import video from './video/video.mp4';
+
+// export default function ResultArea({state,videoPath})
+// {
+//     const [imgState,setimgState]=useState(state)
+//     const [img,setImg]=useState([])
+
+//     useEffect(()=>{
+//         ws.onmessege=(e)=>{
+//             setimgState(true)
+//         }
+//     })
+
+//     useEffect(()=>{
+//         if(imgState)
+//             setImg(VideoFactor(videoPath))
+//     },[imgState])
+
+//     return(
+//         <div style={{
+//             height:'50vh',
+//             width:'80%',
+//             margin:"30px auto",
+//             backgroundColor:'rgba(73,107,191,1.0)',
+//         }}>
+//             {img}
+//         </div>
+//     )
+// }
+
+// function VideoFactor(path)
+// {
+//     return (<video width="100%" height="100%" autoPlay muted>
+//         <source src={video} type="video/mp4"/>
+//     </video>)
+// }
 import { useEffect, useState } from "react";
-import {ws} from "./WebServer"
-import video from './video/video.mp4';
+import { ws } from "./WebServer";
 
-export default function ResultArea({state,videoPath})
-{
-    const [imgState,setimgState]=useState(state)
-    const [img,setImg]=useState([])
+export default function ResultArea({ state, videoPath }) {
+    const [imgState, setImgState] = useState(state);
+    const [img, setImg] = useState(null);
 
-    useEffect(()=>{
-        ws.onmessege=(e)=>{
-            setimgState(true)
+    useEffect(() => {
+        ws.onmessage = (e) => {
+            setImgState(true);
+        };
+    }, []);
+
+    useEffect(() => {
+        if (imgState) {
+            setImg(<VideoFactor path={videoPath} />);
         }
-    })
+    }, [imgState, videoPath]);
 
-    useEffect(()=>{
-        if(imgState)
-            setImg(VideoFactor(videoPath))
-    },[imgState])
-
-    return(
+    return (
         <div style={{
-            height:'50vh',
-            width:'80%',
-            margin:"30px auto",
-            backgroundColor:'rgba(73,107,191,1.0)',
+            height: '50vh',
+            width: '80%',
+            margin: "30px auto",
+            backgroundColor: 'rgba(73,107,191,1.0)',
         }}>
             {img}
         </div>
-    )
+    );
 }
 
-function VideoFactor(path)
-{
-    return (<video width="100%" height="100%" autoPlay muted>
-        <source src={video} type="video/mp4"/>
-    </video>)
+function VideoFactor({ path }) {
+    return (
+        <video key={path} width="100%" height="100%" autoPlay muted>
+            <source src={path} type="video/mp4" />
+        </video>
+    );
 }
